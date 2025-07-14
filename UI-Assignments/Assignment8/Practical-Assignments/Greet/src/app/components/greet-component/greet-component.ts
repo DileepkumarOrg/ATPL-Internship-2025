1cc64d57d9c37ad55e67e12b1215d8365c9ceb5a
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-07-14 14:18:31 +0530

    Greet updated

--- UI-Assignments/Assignment8/Practical-Assignments/Greet/src/app/components/greet-component/greet-component.ts
+++ UI-Assignments/Assignment8/Practical-Assignments/Greet/src/app/components/greet-component/greet-component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
  
 @Component({
   selector: 'app-greet-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './greet-component.html',
   styleUrl: './greet-component.css'
 })
 export class GreetComponent {
-  
+   name: string = '';
+   age: number = 0;
+
+  submit() {
+    if(this.name!="" && this.age >0){
+    window.alert(`Hello ${this.name}, you are ${this.age} years old`);
+    }
+  else{
+    window.alert("Enter Details..!")
+  }
+  }
+
 }
