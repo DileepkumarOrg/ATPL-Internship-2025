fbe8a8bcc61fe8658a42d56e7ecb9ec31f735db8
Dileepkumar74 <dileepkumarrouthu1441@gmail.com>
2025-07-15 06:40:48 +0530

    Updated

--- UI-Assignments/Angular-Practice/FirstExample/My-App/src/app/Component/attribute-directive-component/attribute-directive-component.ts
+++ UI-Assignments/Angular-Practice/FirstExample/My-App/src/app/Component/attribute-directive-component/attribute-directive-component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-attribute-directive-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './attribute-directive-component.html',
   styleUrl: './attribute-directive-component.css'
 })
 export class AttributeDirectiveComponent {
+  className = 'highlight';
+  dynamicClass = 'bordered';
+  classesArray = ['highlight', 'bordered'];
+
+  isActive = true;
+  isDisabled = false;
+  isHighlighted = true;
+
+  isError = true;
+
+  isHighlighted1 = true;
+  isActive1 = true;
 
 }
