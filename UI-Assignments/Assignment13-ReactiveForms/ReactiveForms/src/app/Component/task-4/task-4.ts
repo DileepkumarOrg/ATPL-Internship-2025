f458b35db8b88a8078074fcab48b0e0a722588ca
Dileepkumar74 <dileepkumarrouthu1441@gmail.com>
2025-07-24 06:19:57 +0530

    Assignment13-ReactiveForms updated

--- UI-Assignments/Assignment13-ReactiveForms/ReactiveForms/src/app/Component/task-4/task-4.ts
+++ UI-Assignments/Assignment13-ReactiveForms/ReactiveForms/src/app/Component/task-4/task-4.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-4',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './task-4.html',
   styleUrl: './task-4.css'
 })
 export class Task4 {
+  form : FormGroup = new FormGroup({
+    name : new FormControl('',[Validators.required]),
+    email : new FormControl('',[Validators.required,Validators.email]),
+    address  : new FormGroup({
+      street : new FormControl('',[Validators.required]),
+      city : new FormControl('',[Validators.required]),
+      zip : new FormControl( '',[Validators.required,Validators.minLength(6),Validators.maxLength(6)])
+    }) 
+  })
+  reset(){
+    this.form.reset();
+  }
 
+  onLog(){
+    console.log(this.form.value)
+  }
 }
