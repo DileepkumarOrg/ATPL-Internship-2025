119140bd88bd0e7ee6626ada0c96c7b7d3d94c69
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-08-07 17:38:37 +0530

    Assignment1 is updated

--- Backend-Assignments/Assignment1/LargestNumber.java
+++ Backend-Assignments/Assignment1/LargestNumber.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class LargestNumber{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter length of an Array : ");
+        int lengthArray = s.nextInt();
+        int array[] = new int[lengthArray];
+        for (int j = 0; j<lengthArray; j++){
+            System.out.printf("Enter array[%d] : ", j);
+            array[j] = s.nextInt();
+        }
+        s.close();
+        int max = 0;
+        for (int i = 0; i < array.length ; i++){
+            if(max < array[i]){
+                max = array[i];
+            }
+        }
+        System.out.println("The largest element in the array. : " + max);
     }
 }
+
---
+++ Backend-Assignments/SecondLargestNumber.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SecondLargestNumber {
+    public static int max(int arr[]){
+        int fMax = 0;
+        for (int i = 0; i < arr.length ; i++){
+            if(fMax < arr[i]){
+                fMax = arr[i];
+            }
+        }
+        return fMax;
+    }
+    public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+        System.out.println("Enter length of an Array : ");
+        int lengthArray = s.nextInt();
+        int array[] = new int[lengthArray];
+        for (int j = 0; j<lengthArray; j++){
+            System.out.printf("Enter array[%d] : ", j);
+            array[j] = s.nextInt();
+        }
+        s.close();
+        int firstMaximum = max(array);
+        Arrays.sort(array);
+        System.out.println(array[-1]);
+    }
+}
