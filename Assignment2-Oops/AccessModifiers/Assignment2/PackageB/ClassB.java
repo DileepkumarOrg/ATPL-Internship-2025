c0ab59ea9e61992a514dc8517047bbb8b068c79d
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-08-11 10:46:06 +0530

    Assignment2 is Submitted

--- Assignment2-Oops/AccessModifiers/Assignment2/PackageB/ClassB.java
+++ Assignment2-Oops/AccessModifiers/Assignment2/PackageB/ClassB.java
@@ -1,6 +1,14 @@
 package PackageB;
 import PackageA.*;
+import package1.PublicClass;
 
 public class ClassB {
-    
+    public static void main(String[] args) {
+        PublicClass publicClassObj = new PublicClass();
+        //publicClassObj.defaultMethod();
+        //publicClassObj.protectedMethod();
+
+        // Protect and Default are not accessed outside of the package
+
+    }
 }
